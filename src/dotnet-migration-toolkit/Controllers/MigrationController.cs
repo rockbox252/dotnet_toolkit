47efbcc34d4fd02fcfd97b88fac0584baa8a60c0
Atchyut Preetham Pulavarthi <pulavarthi.preetham@gmail.com>
2021-03-03 11:28:30 +0530

    Add Concurrently & Run dotnet core app from electron app

---
+++ src/dotnet-migration-toolkit/Controllers/MigrationController.cs
@@ -0,0 +1,32 @@
+﻿using dotnet_migration_toolkit.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_migration_toolkit.Controllers
+{
+    [Route("api/v1/")]
+    [ApiController]
+    public class MigrationController : ControllerBase
+    {
+        private readonly IMigrationService _migrationService;
+
+        public MigrationController(IMigrationService migrationService)
+        {
+            _migrationService = migrationService;
+        }
+
+        #region Controller Methods
+        [HttpGet]
+        [Route("migrate")]
+        public async Task<IActionResult> Migrate()
+        {
+            _migrationService.Migrate();
+            return Ok("success");
+        }
+        #endregion
+    }
+}
---
+++ src/dotnet-migration-toolkit/Services/MigrationServices.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_migration_toolkit.Services
+{
+
+    public interface IMigrationService
+    {
+        public void Migrate();
+    }
+
+    public class MigrationService : IMigrationService
+    {
+        #region service methods
+        public void Migrate()
+        {
+            using (var process = new Process())
+            {
+                process.StartInfo.FileName = @$"{Environment.CurrentDirectory}\ApiPort\ApiPort.exe"; // relative path. absolute path works too.
+                process.StartInfo.Arguments = $"analyze -r JSON -f C:/Users/Hp/source/repos/demoproject2";
+                //process.StartInfo.FileName = @"cmd.exe";
+                //process.StartInfo.Arguments = @"/c dir";      // print the current working directory information
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+
+                process.OutputDataReceived += (sender, data) => Console.WriteLine(data.Data);
+                process.ErrorDataReceived += (sender, data) => Console.WriteLine(data.Data);
+                Console.WriteLine("starting");
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+                var exited = process.WaitForExit(1000 * 10);     // (optional) wait up to 10 seconds
+                Console.WriteLine($"exit {exited}");
+            }
+            //return "value";
+        }
+        #endregion 
+
+    }
+}
